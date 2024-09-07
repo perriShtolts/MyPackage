587e0dd8ccca887cfeaa8b4adfe5084d0029cf15
6245755 <6245755@gmail.com>
2024-09-07 18:59:07 +0300

    3

--- mymath/mymath.go
+++ mymath/mymath.go
@@ -5,3 +5,15 @@ import "math"
 func Sqrt(x float64) int {
 	return int(math.Sqrt(x))
 }
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Max(a float64, b float64) float64 {
+	return math.Max(a, b)
+}
+
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
